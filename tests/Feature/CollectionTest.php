0b0f96a692dba2667a47b607f5355c2efad674e6
iniLacher <adijpr25@gmail.com>
2026-03-01 23:41:22 +0700

    belajar manipulasi collection dan forEachnnya

--- tests/Feature/CollectionTest.php
+++ tests/Feature/CollectionTest.php
@@ -11,4 +11,25 @@ class CollectionTest extends TestCase
 
     $this->assertEqualsCanonicalizing([1,2,3, 'inistring'], $collection->all());
   }
+
+  public function testCollectionForEach() {
+    $collection = collect([1,2,3,4,5]);
+
+    foreach ($collection as $key => $value) {
+      $this->assertEquals($key + 1, $value);
+    }
+  }
+
+
+  public function testCrud() {
+    $collection = collect([]);
+
+    $collection->push(1,2,3,4,5);
+    $this->assertEquals([1,2,3,4,5], $collection->all());
+
+
+    $result = $collection->pop();
+    $this->assertEquals(5, $result);
+    $this->assertEquals([1,2,3,4], $collection->all());
+  }
 }
